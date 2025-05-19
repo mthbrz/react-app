9f8b2e88edfa085bf85d0a71491f45df25bb855a
Mathilde <mathilde.bruzat@gmail.com>
2025-05-19 18:22:17 +0200

    réalisation du layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from './pages/home.jsx'
 import About from './pages/about.jsx'
+import Navbar from './components/navbar.jsx'
+import Footer from './components/footer.jsx'
+
+const HeaderFooterLayout = () => {
+  return <>
+<Navbar />
+<Outlet />
+<Footer />  
+  </>
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/flats" element={<h1>Fiche logement</h1>} />
-      <Route path="*" element={<h1>404</h1>} />
+      <Route path="/" element={<HeaderFooterLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/flats" element={<h1>Fiche logement</h1>} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Route>
+
     </Routes>
   )
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import Navbar from '../components/navbar.jsx'
-import Footer from '../components/footer.jsx'
 
 function about() {
   return (
     <div>
-      <Navbar />
       <h1>A propos</h1>
-      <Footer />
     </div>
   )
 }
