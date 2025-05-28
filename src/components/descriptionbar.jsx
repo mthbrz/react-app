f2a96dbbde2e96aa3446d441e05b18818f15b15f
Mathilde <mathilde.bruzat@gmail.com>
2025-05-28 11:26:34 +0200

    finalisation des pages appartment

--- src/components/descriptionbar.jsx
+++ src/components/descriptionbar.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './descriptionbar.css'
 
 function descriptionbar(props) {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const toggleDescription = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const contentClass = (isOpen ? "visible" : "hidden"); + "description-text";
+  const chevronClass = (isOpen ? "fa-solid fa-chevron-up" : "fa-solid fa-chevron-down");
+
   return (
     <div className='equipmentbar'>
       <div className='description-bar'>
         <p className='description-title'>{props.title}</p>
-        <i className="fa-solid fa-chevron-down"></i>
+        <i className={chevronClass} onClick={toggleDescription}></i>
        {/* <i className="fa-solid fa-chevron-up"></i> */}
 
       </div>
 
       <div className='description-content'>
-        <p className="description-text">{props.content}</p>
+        {isOpen && <p className={contentClass}>{props.content}</p>}
       </div>
     </div>
   )
