ab5a17af93cf54cd4aa0d985a1153cab823723ad
Mathilde <mathilde.bruzat@gmail.com>
2025-06-01 10:42:02 +0200

    modification banner

--- src/components/jsx/banner.jsx
+++ src/components/jsx/banner.jsx
@@ -4,21 +4,17 @@ import '../css/banner.css'
 function Banner({ page }) {
   let backgroundImage = ''
   let showText = false
-  let overlayOpacity = 0.5
 
   if (page === 'home') {
     backgroundImage = '/banner.png'
     showText = true
-    overlayOpacity = 0.5
   } else if (page === 'about') {
-    backgroundImage = '/public/banner-about.png'
+    backgroundImage = '/banner-about.png'
     showText = false
-    overlayOpacity = 0.3
   }
 
   return (
     <div className='banner' style={{backgroundImage: `url(${backgroundImage})`,}}>
-      <div className='banner-overlay' style={{backgroundColor: `rgba(0, 0, 0, ${overlayOpacity})`,}}></div>
       {showText && <h1>Chez vous, partout et ailleurs</h1>}
     </div>
   )
