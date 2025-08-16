68a4400839062068bc947888192f94c2261de059
Mathilde <mathilde.bruzat@gmail.com>
2025-08-16 22:38:31 +0200

    build ghPages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css'
 import App from './App.jsx'
@@ -8,8 +8,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/react-app">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
