876a7d1c13a0ebf685f26f8de78e1455023897d5
Mathilde <mathilde.bruzat@gmail.com>
2025-08-16 22:10:42 +0200

    chgt base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-app/",
+  base: "mthbrz.github.io/react-app/",
   build: {
     outDir: 'docs'
   }
